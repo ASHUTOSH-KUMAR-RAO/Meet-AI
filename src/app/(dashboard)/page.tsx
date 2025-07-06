8fb045906e73eccb4dd8c26ae06b8648ce372214
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-07-06 09:41:40 +0530

    08:trpc-setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 
 const Page = async() => {
 
+
   const session = await auth.api.getSession({
     headers: await headers(),
   })
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,30 +1,18 @@
+// How Can We Use trpc Here :- or tenstack query
+
 
 "use client";
 
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 
 export const HomeView = () => {
-    const router = useRouter()
-  const { data: session } = authClient.useSession();
-  if (!session) {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-2xl font-bold">Please sign in to continue</h1>
-      </div>
-    );
-  }
+  const trpc = useTRPC() // Basically useTRPC is a Hool of type trpc
+  const {data}  = useQuery(trpc.hello.queryOptions({text:"Ashutosh Kumar Rao"}))
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">
-        Welcome back, {session.user.name}!
-      </h1>
-      <button
-        className="bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-200"
-        onClick={() => authClient.signOut({fetchOptions:{onSuccess:() => router.push('/sign-in')}})}
-      >
-        Sign Out
-      </button>
+     {data?.greeting}
     </div>
   );
 };
