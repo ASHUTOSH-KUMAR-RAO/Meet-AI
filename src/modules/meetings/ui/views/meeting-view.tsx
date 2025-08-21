f71a8befeca8f15794f9617d2dd5dd57c9ba8ba5
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-08-21 08:14:22 +0530

    16:meetings-setup

---
+++ src/modules/meetings/ui/views/meeting-view.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import {  useSuspenseQuery } from "@tanstack/react-query";
+import React from "react";
+
+const MeetingView = () => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+
+  return <div>{JSON.stringify(data)}</div>;
+};
+
+export default MeetingView;
+
+export const MeetingsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading..."
+      description="Please wait while we fetch your data"
+    />
+  );
+};
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="Access Denied"
+      description="You don't have permission to access this resource."
+    />
+  );
+};
+
+
