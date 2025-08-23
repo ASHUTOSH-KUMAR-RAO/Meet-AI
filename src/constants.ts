73d8d995809f703487b017e20917f76dfe8b9f54
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-08-23 13:20:50 +0530

    20:meetings-filters

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 
 export const DEFAULT_PAGE=1;
-export const DEFAULT_PAGE_SIZE=2;
+export const DEFAULT_PAGE_SIZE=5;
 export const MAXIMUM_PAGE_SIZE=100;
 export const MIN_PAGE_SIZE=1
--- src/modules/meetings/ui/views/meeting-view.tsx
+++ src/modules/meetings/ui/views/meeting-view.tsx
@@ -4,25 +4,44 @@ import { DataTable } from "@/components/data-table";
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
-import {  useSuspenseQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import React from "react";
 import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
+import { useRouter } from "next/navigation";
+import { useMeetingsFilters } from "../../hooks/use-meetings-filter";
+import DataPagination from "@/components/data-pagination";
 
 const MeetingView = () => {
   const trpc = useTRPC();
-  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+  const router = useRouter();
+  const [filters, setFilters] = useMeetingsFilters();
+  const { data } = useSuspenseQuery(
+    trpc.meetings.getMany.queryOptions({
+      ...filters,
+    })
+  );
 
-  return <div className="flex-1 flex pb-4 px-4 md:px-8 flex-col gap-y-4">
-    <DataTable data={data.items} columns={columns}/>
+  return (
+    <div className="flex-1 flex pb-4 px-4 md:px-8 flex-col gap-y-4">
+      <DataTable
+        data={data.items}
+        columns={columns}
+        onRowClick={(row) => router.push(`/meetings/${row.id}`)}
+      />
+      <DataPagination
+        page={filters.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({ page })}
+      />
       {data.items.length === 0 && (
-            <EmptyState
-              title="Create Your First Meeting"
-              description="Shedule a meeting to get started. Each meeting can have multiple agents that can interact with participants during the call.and as well as real time transcription and meeting summary"
-            />
-          )}
-    
-    </div>;
+        <EmptyState
+          title="Create Your First Meeting"
+          description="Shedule a meeting to get started. Each meeting can have multiple agents that can interact with participants during the call.and as well as real time transcription and meeting summary"
+        />
+      )}
+    </div>
+  );
 };
 
 export default MeetingView;
@@ -43,5 +62,3 @@ export const MeetingsViewError = () => {
     />
   );
 };
-
-
